a1cc3938231787da7645dd786b725ae6e4a8e560
Ramadan Ahmed <achmed.ram@gmail.com>
2026-07-31 11:15:06 +0200

    search engine

--- backend/server.js
+++ backend/server.js
@@ -90,7 +90,7 @@ try {
     const authRoutes = require('./routes/auth');
     const newsRoutes = require('./routes/news');
     const adminRoutes = require('./routes/admin');
-     const ogRoutes = require('./routes/og');
+    const ogRoutes = require('./routes/og');
     app.use('/api/auth', authRoutes);
     app.use('/api/news', newsRoutes);
     app.use('/api/admin', adminRoutes);
